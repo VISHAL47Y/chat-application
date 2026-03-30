b7ae17b97da47cf50f3bc11ebd876f45b664edc9
VISHAL47Y <vy479353@gmail.com>
2026-03-30 17:06:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    socket.on("chat message", (data) => {
+        io.emit("chat message", data);
+    });
+});
+
+server.listen(5000, "0.0.0.0", () => {
+    console.log("Server started successfully on http://localhost:5000");
+});
